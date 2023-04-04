5faecae752740f2f115bcd7df8c1ad88cd88ae6d
KeadenM <keadenmeacham@gmail.com>
2023-04-03 20:50:35 -0600

    added API and got it working! (yelp)

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,14 @@
 var buttonPicky = document.getElementById("button1");
+var letsEat = document.getElementById("button2");
 
+const apiKey = 'c4Wu7RmVz4Ug10l8nKrDJzieHkCoFYl6Ba_FBAxoiCeqjgXc8X6B6fJwA57154a2R9fh9SK-YbkPVhBLajXrBJwgxSd1g0knBtlX9-ANJ84rd9LUDyp15G0_iYgrZHYx';
+const options = {method: 'GET', headers: {accept: 'application/json', Authorization: 'Bearer '+apiKey}};
 
-
-
+fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=NYC&sort_by=best_match&limit=20', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+  
 
 
 const successCallback = (position) => {
